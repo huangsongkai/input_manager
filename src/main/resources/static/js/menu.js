7ce3daf3f061093b8f14ac15ec6686f8f5725453
huangsongkai <296731066@qq.com>
2018-02-06 10:13:21 +0800

    黄嵩凯

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,5 +1,5 @@
 var navTree={
-    home:{title:"哈尔滨市双城市欢迎您！",url:"/"},
+    home:{title:"哈尔滨市双城区欢迎您！",url:"/"},
     menu:[{name:"普通投入品管理",children:[{name:"种子购买记录",link:"/seedBuyList"},{name:"农药购买记录",link:"/pesticidesBuyList"},{name:"肥料购买记录",link:"/manureBuyList"}]},
     {name:"高危高毒投入品管理",children:[{name:"购买审批",link:"#"},{name:"购买记录",link:"#"},{name:"票据留存",link:"#"},{name:"使用指导",link:"#"}]},
     {name:"投入品使用监控",children:[{name:"施用记录管理",link:"#"},{name:"自动化采集接口",link:"#"},{name:"施用监控与预警",link:"#"}]},
